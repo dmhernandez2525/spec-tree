e6ae8c86de12f2e61dd1778765b88d8001e6f364
Daniel Hernandez <danielmark252525@GMAIL.COM>
2024-12-10 15:32:58 -0500

    feat: simplify imports in Epic component by consolidating Card imports

--- Client/components/spec-tree/components/epic/epic.tsx
+++ Client/components/spec-tree/components/epic/epic.tsx
@@ -16,13 +16,7 @@ import {
 } from '../../lib/types/work-items';
 import Feature from '../feature';
 import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import {
   Dialog,
   DialogContent,
